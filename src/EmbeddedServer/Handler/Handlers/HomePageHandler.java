fd99cb4fabef355f4896272f4f035005d5a394a8
ProductOfAmerica <jeff.smith34r32@gmail.com>
2017-03-20 04:42:56 -0500

    Added Minified HTML, and added base64 images as CSS instead.

--- src/EmbeddedServer/Handler/Handlers/HomePageHandler.java
+++ src/EmbeddedServer/Handler/Handlers/HomePageHandler.java
@@ -3,12 +3,24 @@ package EmbeddedServer.Handler.Handlers;
 import fi.iki.elonen.NanoHTTPD;
 import fi.iki.elonen.router.RouterNanoHTTPD;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
 public class HomePageHandler extends RouterNanoHTTPD.DefaultHandler {
     public HomePageHandler() {
     }
 
     public String getText() {
-        return "<html><body><h2>Spigot Server Manager is working!</h3></body></html>";
+        try {
+            return new BufferedReader(new InputStreamReader(getClass().
+                    getResourceAsStream("/EmbeddedServer/HTML/index.html")))
+                    .lines().collect(Collectors.joining("\n"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "<html><body><h2>Spigot Server Manager is working!" +
+                "However, you should not be seeing this. Please report this.</h3></body></html>";
     }
 
     public String getMimeType() {
